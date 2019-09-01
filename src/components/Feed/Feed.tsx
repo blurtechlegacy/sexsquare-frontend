94aea4309aa248832817dc70f2a3193e7fa6746c
MacOSO <auroragamesproject@gmail.com>
2019-09-01 21:41:24 +0700

    Add date formatting

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -2,28 +2,33 @@ import React from 'react'
 import nanoid from 'nanoid'
 import AddSexNote from './AddSexNote'
 import styles from './Feed.module.scss'
+import moment from 'moment'
 
 const mockData = [
   {
     nickname: 'MacOSO',
     partners: ['SeagullNina'],
-    timestamp: '',
+    timestamp: '1567343304874',
   },
   {
     nickname: 'SeagullNina',
     partners: ['MacOSO'],
+    timestamp: '1567340306874',
   },
   {
     nickname: 'Jane',
     partners: ['John', 'Bob', 'Eugene'],
+    timestamp: '1567303204874',
   },
   {
     nickname: 'Bob',
     partners: ['Eugene', 'Jane'],
+    timestamp: '1567043202874',
   },
   {
     nickname: 'John',
     partners: [],
+    timestamp: '1565000004874',
   },
 ]
 
@@ -31,17 +36,14 @@ const punches = [
   ['beat the meat out of'],
   ['knocked n-times into'],
   ['had sex with'],
-  ['']
-];
+]
+
+const dateFormat = (date: string) => moment(Number(date)).fromNow()
 
 const getRandomInt = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min)) + min
 
 const FeedMenu = () => {
-  function getRandomInt(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
   return (
     <div className={styles.feedContainer}>
       <AddSexNote />
@@ -49,15 +51,20 @@ const FeedMenu = () => {
         {mockData.map(i => {
           return (
             <div key={nanoid(8)} className={styles.sexNote}>
-              <div className={styles.message}>
-                {i.nickname}{' '}
-                {i.partners.length > 0
-                  ? `${
-                      punches[getRandomInt(0, punches.length)]
-                    } ${i.partners.map(
-                      (item, index) => (index ? ' ' : '') + item
-                    )}`
-                  : 'free yourself'}
+              <div className={styles.note}>
+                <div className={styles.message}>
+                  {i.nickname}{' '}
+                  {i.partners.length > 0
+                    ? `${
+                        punches[getRandomInt(0, punches.length)]
+                      } ${i.partners.map(
+                        (item, index) => (index ? ' ' : '') + item
+                      )}`
+                    : 'free yourself'}
+                </div>
+                <div className={styles.timestamp}>
+                  {dateFormat(i.timestamp)}
+                </div>
               </div>
             </div>
           )
--- src/components/Places.tsx
+++ src/components/Places.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header/Header'
-import Map from './components/Map/Map'
+import Header from './Header/Header'
+import Map from './Map/Map'
 
 const App: React.FC = () => {
   return (
