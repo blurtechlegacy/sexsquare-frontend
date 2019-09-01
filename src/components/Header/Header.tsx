f36865e2926b66951ca20fbd9e672df9a9269c24
MacOSO <auroragamesproject@gmail.com>
2019-09-01 18:28:31 +0700

    Add new global vars and rewrite Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -8,80 +7,16 @@ import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        grow: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            display: 'none',
-            [theme.breakpoints.up('sm')]: {
-                display: 'block',
-            },
-        },
-        search: {
-            position: 'absolute',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            '&:hover': {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
-            },
-            left: '16.66%',
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(3),
-                width: 'auto',
-            },
-        },
-        searchIcon: {
-            width: theme.spacing(7),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        inputRoot: {
-            color: 'inherit',
-        },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 7),
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('md')]: {
-                width: 200,
-            },
-        },
-        sectionDesktop: {
-            display: 'none',
-            [theme.breakpoints.up('md')]: {
-                display: 'flex',
-            },
-        },
-        sectionMobile: {
-            display: 'flex',
-            [theme.breakpoints.up('md')]: {
-                display: 'none',
-            },
-        },
-    }),
-);
+import styles from './Header.module.scss'
 
 export default function PrimarySearchAppBar() {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | undefined>(undefined);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<HTMLElement | undefined>(undefined);
 
     const isMenuOpen = Boolean(anchorEl);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -91,11 +26,11 @@ export default function PrimarySearchAppBar() {
     };
 
     const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
+        setMobileMoreAnchorEl(undefined);
     };
 
     const handleMenuClose = () => {
-        setAnchorEl(null);
+        setAnchorEl(undefined);
         handleMobileMenuClose();
     };
 
@@ -161,7 +96,7 @@ export default function PrimarySearchAppBar() {
     );
 
     return (
-        <div className={classes.grow}>
+        <div className={styles.grow}>
             <AppBar position="static">
                 <Toolbar>
                     {/*}<IconButton
@@ -173,24 +108,24 @@ export default function PrimarySearchAppBar() {
                     >
                         <MenuIcon />
                     </IconButton>*/}
-                    <Typography className={classes.title} variant="h6" noWrap>
+                    <Typography className={styles.title} variant="h6" noWrap>
                         SexSquare
                     </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
+                    <div className={styles.search}>
+                        <div className={styles.searchIcon}>
                             <SearchIcon />
                         </div>
                         <InputBase
                             placeholder="Search…"
                             classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
+                                root: styles.inputRoot,
+                                input: styles.inputInput,
                             }}
                             inputProps={{ 'aria-label': 'search' }}
                         />
                     </div>
-                    <div className={classes.grow} />
-                    <div className={classes.sectionDesktop}>
+                    <div className={styles.grow} />
+                    <div className={styles.sectionDesktop}>
                         <IconButton aria-label="show 4 new mails" color="inherit">
                             <Badge badgeContent={4} color="secondary">
                                 <MailIcon />
@@ -212,7 +147,7 @@ export default function PrimarySearchAppBar() {
                             <AccountCircle />
                         </IconButton>
                     </div>
-                    <div className={classes.sectionMobile}>
+                    <div className={styles.sectionMobile}>
                         <IconButton
                             aria-label="show more"
                             aria-controls={mobileMenuId}
