00ed6afd8ad299c6f80389c9ad55ffebde122af8
tnnNull <vetka921@gmail.com>
2019-09-01 16:38:38 +0700

    SCSS added

--- src/Feed/FeedMenu.tsx
+++ src/Feed/FeedMenu.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Icon from '@material-ui/core/Icon';
+import './feedStyles.scss';
+
+console.log('ok');
+const a = [0,1,2,3,4,5];
 
 const FeedMenu = () => {
+
     return (
-        <div>
+        <div className="listContainer">
+          {a.map(i => {
+            return <div className="sexNote"></div>
+          })}
+
         </div>
     )
 };
