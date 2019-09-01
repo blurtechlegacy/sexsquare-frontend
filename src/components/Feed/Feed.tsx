b93d543f3c12e9cd27597d09671b9c48ba8b815f
MacOSO <auroragamesproject@gmail.com>
2019-09-01 18:58:19 +0700

    Added prettier and stylint

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import nanoid from "nanoid";
-import AddSexNote from "./AddSexNote";
-import styles from './Feed.module.scss';
+import React from 'react'
+import nanoid from 'nanoid'
+import AddSexNote from './AddSexNote'
+import styles from './Feed.module.scss'
 
-const a = [0,1,2,3,4,5];
+const mockData = [
+  {
+    nickname: 'MacOSO',
+    partners: ['SeagullNina', 'MacOSO'],
+  },
+];
 
 const FeedMenu = () => {
-
-    return (
-      <div className={styles.feedContainer}>
-        <AddSexNote />
-        <div className={styles.listContainer}>
-          {a.map(i => {
-            return <div key={nanoid(8)} className={styles.sexNote}>{i}</div>
-          })}
-        </div>
+  return (
+    <div className={styles.feedContainer}>
+      <AddSexNote />
+      <div className={styles.listContainer}>
+        {mockData.map(i => {
+          return (
+            <div key={nanoid(8)} className={styles.sexNote}>
+              {i.nickname} have sex with{' '}
+              {i.partners.map((item, index) => (index ? ', ' : '') + item)}
+            </div>
+          )
+        })}
       </div>
-    )
-};
+    </div>
+  )
+}
 
 export default FeedMenu
