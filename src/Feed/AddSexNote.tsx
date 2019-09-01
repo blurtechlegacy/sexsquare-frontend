f39e3092ef2aa4b9a21a75b34c461b52f5f24bbd
MacOSO <auroragamesproject@gmail.com>
2019-09-01 17:17:31 +0700

    Add AddSexNote styles

--- src/Feed/AddSexNote.tsx
+++ src/Feed/AddSexNote.tsx
@@ -1,61 +1,33 @@
 import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Icon from '@material-ui/core/Icon';
-import './feedStyles.scss';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        container: {
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        button: {
-            margin: theme.spacing(1),
-        },
-        buttons: {
-            display: 'flex',
-            justifyContent: 'space-between',
-        },
-        rightIcon: {
-            marginLeft: theme.spacing(1),
-        },
-        title: {
-            display: 'flex',
-        },
-        input: {
-            margin: theme.spacing(1),
-        },
-    }),
-);
+import styles from './AddSexNote.module.scss'
 
 const AddSexNote = () => {
-    const classes = useStyles();
-
     return (
-        <div className={classes.container}>
-            <h1 className={classes.title}>Add sex note</h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Add sex note</h1>
             <Input
                 placeholder="Where have you had sex?"
                 defaultValue=""
-                className={classes.input}
+                className={styles.input}
                 inputProps={{
                     'aria-label': 'description',
                 }}
             />
-            <div className={classes.buttons}>
+            <div className={styles.buttons}>
             <FormControlLabel
                 value="start"
                 control={<Checkbox color="primary" />}
                 label="Private"
                 labelPlacement="start"
             />
-            <Button variant="contained" color="primary" className={classes.button}>
+            <Button variant="contained" color="primary" className={styles.button}>
                 Send
-                <Icon className={classes.rightIcon}>send</Icon>
+                <Icon className={styles.rightIcon}>send</Icon>
             </Button>
             </div>
         </div>
